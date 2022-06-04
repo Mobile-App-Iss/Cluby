0dfbd6e9e6569c7cc033b5e5e75b598f1027406a
Rafik El Ghali <77727378+Rafik-ElGhali@users.noreply.github.com>
2022-06-04 07:23:02 +0100

    Add files via upload

---
+++ client-Side/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import RootNavigation from "./navigation";
+
+
+export default function App() {
+ return <RootNavigation />;
+   
+  
+  
+}
+
+
+
+
+/*const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+*/
---
+++ client-Side/confing.js
@@ -0,0 +1,1 @@
+export const API="https://four-moments-hammer-197-31-186-194.loca.lt/api";
